9550cc7332bce58913d5d266e6e96ca1e14dd3d7
rrahool <rbiswas596@gmail.com>
2020-03-30 16:09:18 +0600

    Retrieve data from local storage, set cart from saved state, Error Showing cart.length

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
 
 const Review = () => {
+
+    const {cart, setCart} = useState([]);
+    
+    useEffect( () => {
+        //cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+
+        const cartProducts = productKeys.map( key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        // console.log(cartProducts);
+        setCart(cartProducts);
+    });
+    
     return (
         <div>
-            <h1>This is Review</h1>
+            <h1>Cart Items: {cart.length}</h1>
         </div>
     );
 };
