a5c68bb865d9c3d08f1a7a9c3fbda33e86c3c26f
rrahool <rbiswas596@gmail.com>
2020-03-30 16:50:57 +0600

    Remove from cart, pass parameter on click, remove from state

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
 
     const [cart, setCart] = useState([]);
+
+    const removeProduct = (productKey) => {
+        console.log('remove clicked', productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
     
     useEffect( () => {
         //cart
@@ -29,6 +36,7 @@ const Review = () => {
                 cart.map( 
                     pd => <ReviewItem 
                                         key = {pd.key}
+                                        removeProduct = {removeProduct}
                                         product={pd}>
                                 </ReviewItem>)
             }
