c0073dabaade80325b796adec6f478bf56056a17
rrahool <rbiswas596@gmail.com>
2020-03-30 17:47:20 +0600

    React Developer Tool, Chrome extension, Refactor add to Cart for Shop.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,6 +15,7 @@ const Cart = (props) => {
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
         total = total + product.price * product.quantity;
+        // debugger;
 
     }
 
