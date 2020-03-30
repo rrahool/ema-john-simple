593cd0f45f5437b142bf97760411ac526bd92f6d
rrahool <rbiswas596@gmail.com>
2020-03-30 17:04:12 +0600

    Share components among multiple routes, share data on cart

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import Cart from '../Cart/Cart';
 
 const Review = () => {
 
@@ -30,16 +31,20 @@ const Review = () => {
     }, []);
     
     return (
-        <div>
-            <h1>Cart Items: {cart.length}</h1>
-            {
-                cart.map( 
-                    pd => <ReviewItem 
-                                        key = {pd.key}
-                                        removeProduct = {removeProduct}
-                                        product={pd}>
-                                </ReviewItem>)
-            }
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    cart.map( 
+                        pd => <ReviewItem 
+                                            key = {pd.key}
+                                            removeProduct = {removeProduct}
+                                            product={pd}>
+                                    </ReviewItem>)
+                }
+            </div>
+            <div className="cart-contianer">
+                <Cart cart={cart}></Cart>
+            </div>            
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -23,7 +23,7 @@ const Shop = () => {
     }
 
     return (
-        <div className="shop-container">
+        <div className="twin-container">
             <div className="product-container">
                 <ul>
                     {
