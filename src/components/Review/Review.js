dd144fd48f64373ae49a09623314fdcf0d5ae080
rrahool <rbiswas596@gmail.com>
2020-03-30 16:34:41 +0600

    Review Component, Review Items, CSS in Javascript style

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
 
-    const {cart, setCart} = useState([]);
+    const [cart, setCart] = useState([]);
     
     useEffect( () => {
         //cart
@@ -19,11 +20,18 @@ const Review = () => {
         });
         // console.log(cartProducts);
         setCart(cartProducts);
-    });
+    }, []);
     
     return (
         <div>
             <h1>Cart Items: {cart.length}</h1>
+            {
+                cart.map( 
+                    pd => <ReviewItem 
+                                        key = {pd.key}
+                                        product={pd}>
+                                </ReviewItem>)
+            }
         </div>
     );
 };
